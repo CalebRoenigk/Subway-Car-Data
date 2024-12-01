64344d547d97b7d01f8cf6bd1035a26ea366faf9
CalebRoenigk <69256517+CalebRoenigk@users.noreply.github.com>
2024-12-01 11:19:33 -0500

    Removal of Test
    Ignore me!!!

--- assets/js/main.js
+++ assets/js/main.js
@@ -166,9 +166,6 @@ function graphPointsByNumber() {
         point.style.top = yPos;
         point.style.left = xPos;
     }
-    
-    console.log(allRecords[0]["['fields']['Number']"]);
-    
 
     setMinMaxLabels(minMax);
 }
